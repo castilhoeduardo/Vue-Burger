107abb03c8600ee9ac280fe3d6be0552fb36ad59
Eduardo Castilho Barbosa <alcantara.ecb@gmail.com>
2023-06-30 14:44:54 -0300

    ..

---
+++ src/components/Banner.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="main-banner">
+        <h1>Make Your Burguer</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Banner"
+
+}
+</script>
+
+<style scoped>
+
+    #main-banner {
+        background-image: url('/public/img/burger.jpg');
+        background-position: 0 -250px;
+        background-size: cover;
+        height: 600px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+
+    #main-banner h1 {
+        color: #b3842d; 
+        background-color: #222;
+        font-size: center;
+        font-size: 60px;
+       
+        padding: 20px 40px;
+        }
+</style>
